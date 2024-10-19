a833b7ee461739fc4e9e51c60a268f6add25bf46
IsmaelDawuda <49082846+IsmaelDawuda@users.noreply.github.com>
2024-10-19 11:38:05 -0500

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from FlaskWebProject import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8000'))
+        PORT = int(environ.get('PORT', '8000'))  # Changed to 'PORT'
     except ValueError:
         PORT = 8000
     app.run(HOST, PORT, ssl_context='adhoc')
